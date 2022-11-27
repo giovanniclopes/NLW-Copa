117fdbcabd225c8d51f34d125f3dc60a9525704d
Giovanni Lopes <giovanniclopes@gmail.com>
2022-11-27 19:19:42 -0300

    tables created with 'Prisma'

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,9 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -7,8 +12,10 @@ async function bootstrap() {
 
   // hhtp??localhost:3333/pools/count
 
-  fastify.get('/pools/count', () => {
-    return { count: 0 }
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+
+    return { count }
   })
 
   await fastify.listen({ port: 3333 })
